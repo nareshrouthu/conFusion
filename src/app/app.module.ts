61e0f0a3f41692ec0fa50800a823c6e75f964f86
Naresh Routhu <>
2020-07-20 15:51:32 +0530

    second commit with annimation dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import{BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import{MatToolbar} from '@angular/material/toolbar';
+import{FlexLayoutModule} from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbar
   ],
   providers: [],
   bootstrap: [AppComponent]
